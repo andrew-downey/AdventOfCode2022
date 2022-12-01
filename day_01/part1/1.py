7a0c09e4196186eddd19be2f6f4a7ddbabcb6108
Andrew Downey <andrew.downey@wearepatchworks.com>
2022-12-01 10:30:40 +0000

    Day 1! Used a calculator for part 2 because why not

--- day_01/part1/1.py
+++ day_01/part1/1.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 import os
 from time import time
+from collections import defaultdict
 
 
 def main():
     dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, '../example.txt')
+    filename = os.path.join(dirname, '../input.txt')
     with open(filename, "r") as myfile:
         rows = myfile.read().splitlines()
 
-    print("Rows: {0}".format(rows))
+    current_elf = 1
+    elves = defaultdict(int)
+    for row in rows:
+        if (row == ''):
+            current_elf += 1
+            continue
+        elves[current_elf] += int(row)
+
+    elves = {key: val for key, val in sorted(
+        elves.items(), key=lambda ele: ele[1], reverse=True)}
+
+    print("Elves: {0}".format(elves))
 
 
 if __name__ == "__main__":
