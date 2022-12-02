bf1c5b17e5b4bcef0eeb92f7205e05e0a507f7bc
Andrew Downey <andrew.downey@wearepatchworks.com>
2022-12-02 11:14:00 +0000

    Adding day 2

---
+++ day_02/part1/1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import os
+from time import time
+
+playmap = {
+    'X': 1,
+    'Y': 2,
+    'Z': 3
+}
+
+winmap = {
+    'A': 'Y',
+    'B': 'Z',
+    'C': 'X'
+}
+
+drawmap = {
+    'A': 'X',
+    'B': 'Y',
+    'C': 'Z'
+}
+
+
+def main():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, '../input.txt')
+    with open(filename, "r") as myfile:
+        rows = myfile.read().splitlines()
+
+    score = 0
+    for row in rows:
+        row = row.split(' ')
+        score += playmap[row[1]]
+
+        if winmap.get(row[0]) == row[1]:
+            score += 6
+        elif drawmap.get(row[0]) == row[1]:
+            score += 3
+
+    print("Score: {0}".format(score))
+
+
+if __name__ == "__main__":
+    start = time()
+    main()
+    end = time()
+    print(f'Finished in {round(end - start, 2)} seconds')
---
+++ day_02/part2/2.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import os
+from time import time
+
+playmap = {
+    'X': 1,
+    'Y': 2,
+    'Z': 3
+}
+
+winmap = {
+    'A': 'Y',
+    'B': 'Z',
+    'C': 'X'
+}
+
+drawmap = {
+    'A': 'X',
+    'B': 'Y',
+    'C': 'Z'
+}
+
+losemap = {
+    'A': 'Z',
+    'B': 'X',
+    'C': 'Y'
+}
+
+
+def main():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, '../input.txt')
+    with open(filename, "r") as myfile:
+        rows = myfile.read().splitlines()
+
+    score = 0
+    for row in rows:
+        row = row.split(' ')
+        my_play = ''
+        if row[1] == 'X':
+            my_play = losemap.get(row[0])
+        elif row[1] == 'Y':
+            my_play = drawmap.get(row[0])
+        else:
+            my_play = winmap.get(row[0])
+
+        score += playmap[my_play]
+
+        if winmap.get(row[0]) == my_play:
+            score += 6
+        elif drawmap.get(row[0]) == my_play:
+            score += 3
+
+    print("Score: {0}".format(score))
+
+
+if __name__ == "__main__":
+    start = time()
+    main()
+    end = time()
+    print(f'Finished in {round(end - start, 2)} seconds')
