681d4d6ae1c20c2a51d013f05c906236eeba03c4
Andrew Downey <andrew.downey@wearepatchworks.com>
2022-12-05 14:47:11 +0000

    Day 5, it's all popping off. Wrote it with deque's in mind but wasn't needed in the end.

---
+++ day_05/part1/1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import os
+from time import time
+import re
+from collections import defaultdict
+from collections import deque
+
+
+def main():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, '../input.txt')
+    with open(filename, "r") as myfile:
+        rows = myfile.read().splitlines()
+
+    stacks = defaultdict(deque)
+    for row in rows:
+        if row.startswith('move'):
+            command = [int(x) for x in re.findall(r"(\d+)", row)]
+            for _ in range(1, command[0] + 1):
+                if (len(stacks[command[1]]) > 0):
+                    crate = stacks[command[1]].pop()
+                    stacks[command[2]].append(crate)
+        elif len(row) > 0 and not row.startswith(' 1'):
+            crates = re.findall(r"(\w|\s\s\s\s)", row)
+            for crate_id in range(len(crates)):
+                if len(crates[crate_id]) == 1:
+                    stacks[crate_id + 1].appendleft(crates[crate_id])
+
+    stacks = {key: val for key, val in sorted(
+        stacks.items(), key=lambda ele: ele[0])}
+    print(stacks)
+
+    string = ''
+    for i in range(1, len(stacks) + 1):
+        if (len(stacks[i]) > 0):
+            string += stacks[i].pop()
+    print(string)
+
+
+if __name__ == "__main__":
+    start = time()
+    main()
+    end = time()
+    print(f'Finished in {round((end - start) * 1000, 5)} milliseconds')
