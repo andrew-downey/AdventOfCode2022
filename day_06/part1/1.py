84e7ee28be7f7870e5caf8694884d0be532aace6
Andrew Downey <andrew.downey@wearepatchworks.com>
2022-12-07 15:25:24 +0000

    Day 6 complete, only a day late

--- day_06/part1/1.py
+++ day_06/part1/1.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
 import os
 from time import time
+from collections import deque
 
 
 def find_marker(input: str):
-    print(input)
+    for i in range(3, len(input) - 4):
+        buff = input[i-4:i]
+        count = sum([buff.count(char) for char in buff])
+        if count == 4:
+            return i
+
+    return 0
 
 
 def main():
+    assert find_marker('bvwbjplbgvbhsrlpgdmjqwftvncz') == 5
+    assert find_marker('nppdvjthqldpwncqszvftbrmjlhg') == 6
+    assert find_marker('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg') == 10
+    assert find_marker('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw') == 11
+
     dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, '../example.txt')
+    filename = os.path.join(dirname, '../input.txt')
     with open(filename, "r") as myfile:
         rows = myfile.read().splitlines()
 
     for row in rows:
-        find_marker(row)
-
-    print(f"Rows: {rows}")
+        marker = find_marker(row)
+        print(marker)
 
 
 if __name__ == "__main__":
--- day_06/part2/2.py
+++ day_06/part2/2.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 import os
 from time import time
+from collections import deque
+
+
+def find_marker(input: str):
+    for i in range(13, len(input)):
+        buff = input[i-14:i]
+        count = sum([buff.count(char) for char in buff])
+        if count == 14:
+            return i
+
+    return 0
 
 
 def main():
     dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, '../example.txt')
+    filename = os.path.join(dirname, '../input.txt')
     with open(filename, "r") as myfile:
         rows = myfile.read().splitlines()
 
-    print(f"Rows: {rows}")
+    for row in rows:
+        marker = find_marker(row)
+        print(marker)
 
 
 if __name__ == "__main__":
