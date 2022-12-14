41779de5e2edb25103cc5b91e742bf7d7d8192a1
Andrew Downey <andrew@anooserve.com>
2022-12-14 00:41:35 +0000

    Moving to djikstras as my new algo wasn't working. Copied from last year for now but needs cleaned up

--- day_12/part1/1.py
+++ day_12/part1/1.py
@@ -1,32 +1,62 @@
 #!/usr/bin/env python3
-import math
 import os
 from time import time
 from colorama import Style
 
 start: tuple[int, int]
-end: tuple[int, int]
-def explore(steps: int, grid: dict[int, dict[int, int]], current_pos: tuple[int, int]):
-    global start
-    global end
-    if current_pos == end or current_pos == start:
-        return steps
+target_point: tuple[int, int]
+global_paths: list[tuple[int, int]] = []
 
-    if steps > 200:
-        return None
+def explore(grid: dict[int, dict[int, int]], current_pos: tuple[int, int]):
+    visited.append(current_pos)
 
-    steps += 1
-    next_neighbours = [(y, x) for y, i in grid.items() for x, j in i.items() if math.abs(y - current_pos[0] <= 1 or math.abs(x - current_pos[1] <= 1))]
+    #     for y, i in grid.items():
+    #         for x, j in i.items():
+    #             tup = (y, x)
+    #             print(f"{Style.BRIGHT if (tup in visited) else Style.DIM} {'#' if (tup in visited) else '.'}{Style.RESET_ALL}", end='')
+    #         print()
+    #     print()
 
-    paths = []
-    for pos in next_neighbours:
-        paths.append(explore(steps, grid, pos))
+    y, x = current_pos
+    current_height = grid[y][x]
+    for ny, nx in [(y - 1, x), (y + 1, x), (y, x - 1), (y, x + 1)]:
+        if (ny, nx) not in visited and nx >= 0 and ny >= 0 and nx < len(grid[0]) and ny < len(grid):
+            new_height = grid[ny][nx]
+            if abs(current_height - new_height) <= 1:
+                queue.append((ny, nx))
 
-    return paths
+    global target_point
+    visited = {}
+    queue = {(0, 0): 0}
+    while len(queue) > 0:
+            # cache stack is empty or invalid, nullify it and find the minimum
+            current_cost = min(queue.values())
+            minimums_cache_stack = []
+            current_cost_index = list(queue.values()).index(current_cost)
+            current_node = list(queue.keys())[current_cost_index]
+
+        for neighbour, cost in graph[current_node].items():
+            if neighbour not in visited:
+                new_cost = MAXINT
+                try:
+                    new_cost = queue[neighbour]
+                except KeyError:
+                    pass
+                candidate_cost = current_cost + cost
+                if candidate_cost < new_cost:
+                    queue[neighbour] = candidate_cost
+                    if new_cost <= current_minimum:
+                        current_minimum = new_cost
+
+        visited[current_node] = current_cost
+        del queue[current_node]
+
+        if(current_node == target_point):
+            break
 
 def main():
     dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, '../example.txt')
+    filename = os.path.join(dirname, '../input.txt')
     with open(filename, "r") as myfile:
         rows = myfile.read().splitlines()
 
@@ -49,15 +79,28 @@ def main():
 
             grid[y][x] = value
 
+    print("Initial map")
     for y, i in grid.items():
         for x, j in i.items():
             tup = (y, x)
             print(f"{Style.BRIGHT if (tup == end or tup == start) else Style.DIM} {j:>3}{Style.RESET_ALL}", end='')
         print()
-
-    explore(0, grid, start)
-
     print(Style.RESET_ALL)
+
+    explore(grid, start, [])
+
+    global global_paths
+    global_paths = sorted(global_paths, key=lambda path: len(path))
+
+    # [print(f"{len(path)} {path}") for path in global_paths]
+    for path in range(min(len(global_paths), 3)):
+        print(f"{path} ({len(global_paths[path] - 1)} steps)")
+        for y, i in grid.items():
+            for x, j in i.items():
+                tup = (y, x)
+                print(f"{Style.BRIGHT if (tup in global_paths[path]) else Style.DIM} {j:>3}{Style.RESET_ALL}", end='')
+            print()
+        print()
 
 
 
